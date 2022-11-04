e4717c067281325b178fa19164ab554364a6dbcb
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-11-04 20:44:47 +0100

    init

---
+++ src/main/java/pl/edu/pwr/bezp/communicator2/Communicator2Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.bezp.communicator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Communicator2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Communicator2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pwr/bezp/communicator2/Communicator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.bezp.communicator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Communicator2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
